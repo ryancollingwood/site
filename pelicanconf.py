f03e264b8e6b412d62f79def54001fdbd3a66c19
ryancollingwood <ryancollingwood@gmail.com>
2019-01-03 15:38:51 +1100

    update SITE_URL to https

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Ryan Collingwood'
 SITENAME = 'ryancollingwood.info'
-SITEURL = 'http://ryancollingwood.info/'
+SITEURL = 'https://ryancollingwood.info'
 
 PATH = 'content'
 
